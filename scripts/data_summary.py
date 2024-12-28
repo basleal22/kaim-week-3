4cddb613a35c23439bb2b574e4a7532461bda758
basleal22 <solibaslu@gmail.com>
2024-12-28 10:47:12 +0300

    added a function for the univariate function

--- scripts/data_summary.py
+++ scripts/data_summary.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 def col_data_summary(data):#check for variability for the numerical columns
     rows=[]
     numeric_column= data.select_dtypes(include=['int64','float64'])
@@ -24,4 +25,40 @@ def data_types(data):
     bool_var=data.select_dtypes(include=['bool'])
     # Combine identified columns into a set
     return categorical_variables.columns.tolist(),numerical_variables.columns.tolist(), date_variables.columns.tolist(),bool_var.columns.tolist()
-    
+def missing_values_handle(data):
+    title_to_gender={'Mr':'Male',
+                     'Mrs':'Female',
+                     'Ms':'Female',
+                     'Miss':'Female',
+                     }
+    numerical_cols=data.select_dtypes(include=['int64','float64']).columns
+    categorical_cols=data.select_dtypes(include=['object','category']).columns
+    for column in data.columns:
+        if column in numerical_cols:
+            data[column]=data[column].fillna(data[column].mean())
+        if column == 'Gender':
+            data['Gender']=data['Gender'].fillna(data['Title'].map(title_to_gender))
+        if column in categorical_cols:
+            data.fillna(data[column].mode().iloc[0])
+    return data
+
+def num_cat_visual(data):
+    #perform univariate analysis on the numerical and categorial columns
+    categorical_cols=data.select_dtypes(include=['object','category'])
+    numerical_cols=data.select_dtypes(include=['int64','float64'])
+    #plot histogram for numerical columns
+    plt.figure(figsize=(10,8))
+    for col in numerical_cols:
+        sns.histplot(data[col],kde=True,color='blue')
+    plt.title(f'distribution solumns')
+    plt.xlabel(col)
+    plt.ylabel('frequency')
+    plt.show()
+    #plot bar charts for categorical columns
+    plt.figure(figsize=(8,6))
+    for col in categorical_cols:
+        data[col].value_counts().plot(kind='bar',color= 'green')#Use the value_counts() method to calculate the frequency of each category.
+    plt.title(f'distribution of columns')
+    plt.xlabel('columns')
+    plt.ylabel('count')
+    plt.show()
